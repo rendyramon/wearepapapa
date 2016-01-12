c8f095efc6c6ab1cc9e664bfea5b9638d1ace447
Rongzhiliu <liurongzhi0817@163.com>
2016-01-12 14:41:12 +0800

    同步2

---
+++ app/src/main/java/com/hotcast/vr/pageview/SplashView.java
@@ -0,0 +1,14 @@
+package com.hotcast.vr.pageview;
+
+import com.hotcast.vr.BaseActivity;
+import com.hotcast.vr.R;
+
+/**
+ * 欢迎页面的  view
+ * Created by lostnote on 16/1/12.
+ */
+public class SplashView extends BaseView {
+    public SplashView(BaseActivity activity) {
+        super(activity, R.layout.activity_splash);
+    }
+}
