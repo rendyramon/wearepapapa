eefaa3d6212d19bf545f4a419a22438caae99a72
zhangjunjun <835184840@qq.com>
2016-05-10 16:13:42 +0800

    在获取本地视频列表之前缓存视频图片

--- app/src/main/java/com/hotcast/vr/asynctask/LocalVideosAsynctask.java
+++ app/src/main/java/com/hotcast/vr/asynctask/LocalVideosAsynctask.java
@@ -1,7 +1,12 @@
 package com.hotcast.vr.asynctask;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
 
 import com.hotcast.vr.BaseApplication;
 import com.hotcast.vr.bean.LocalBean2;
@@ -11,8 +16,14 @@ import com.hotcast.vr.tools.VedioBitmapUtils;
 import com.lidroid.xutils.DbUtils;
 import com.lidroid.xutils.exception.DbException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.graphics.BitmapFactory.*;
 
 /**
  * Created by liurongzhi on 2016/2/2.
@@ -31,6 +42,7 @@ public class LocalVideosAsynctask extends AsyncTask<Integer, Integer, List<Local
 
     @Override
     protected List doInBackground(Integer... params) {
+        saveLocalVideoImage();
         DbUtils db = DbUtils.create(context);
         try {
             dbList = db.findAll(LocalBean2.class);
@@ -97,6 +109,57 @@ public class LocalVideosAsynctask extends AsyncTask<Integer, Integer, List<Local
         BaseApplication.doAsynctask = true;
         return dbList;
     }
+
+    public void saveLocalVideoImage(){
+        final Options options = new Options();
+        options.inDither = false;
+        switch (options.inPreferredConfig = Bitmap.Config.ARGB_8888) {
+        }
+        final ContentResolver contentResolver = context.getContentResolver();
+        String[] projection = new String[] {  MediaStore.Video.Media.DATA,MediaStore.Video.Media.SIZE ,MediaStore.Video.Media._ID,MediaStore.Video.Media.DISPLAY_NAME};
+        final Cursor cursor = contentResolver.query(
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, MediaStore.Video.Media.MIME_TYPE + "='video/mp4'",
+                null, MediaStore.Video.Media.DEFAULT_SORT_ORDER);
+        cursor.moveToFirst();
+        int fileNum = cursor.getCount();
+        for (int counter = 0; counter < fileNum; counter++) {
+            final String path=cursor.getString(0);
+            long size=Long.parseLong(cursor.getString(1))/(1024*1024);
+            if(size>10&& !path.contains("/hostcast/vr/")){
+                final String VideoName=cursor.getString(3);
+                final long videoId=Long.parseLong(cursor.getString(2));
+
+                final Bitmap bitmap = MediaStore.Video.Thumbnails.getThumbnail(contentResolver, videoId,
+                        MediaStore.Images.Thumbnails.MINI_KIND, options);
+                saveBitmap(VideoName.replace(".mp4",".jpg"),bitmap);
+            }
+
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+    }
+    public void saveBitmap(String name,Bitmap bitmap) {
+        File f = new File(BaseApplication.ImgCacheUrl, name);
+        if (f.exists()) {
+            return;
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
 
     @Override
     protected void onPostExecute(List<LocalBean2> s) {
